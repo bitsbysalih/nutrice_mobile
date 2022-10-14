0d25268d4c79c10a050b8d1bb0d56360c0dc59c5
bitsbysalih <salih.codes@gmail.com>
2022-10-14 09:02:03 +0300

    initial commit

---
+++ lib/routes/router.dart
@@ -0,0 +1,22 @@
+import '../../resources/pages/home_page.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+
+/*
+|--------------------------------------------------------------------------
+| App Router
+|
+| * [Tip] Create pages faster 🚀
+| Run the below in the terminal to create new a page.
+| "flutter pub run nylo_framework:main make:page my_page"
+| Learn more https://nylo.dev/docs/3.x/router
+|--------------------------------------------------------------------------
+*/
+
+appRouter() => nyRoutes((router) {
+
+  router.route("/", (context) => MyHomePage(title: "Hello World"));
+
+  // Add your routes here
+
+  // router.route("/new-page", (context) => NewPage(), transition: PageTransitionType.fade);
+});
